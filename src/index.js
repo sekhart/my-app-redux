301f3ff3da2c9bb646b1a8a36574351f5f2c6154
eunike <eunike_tarigan@msi-global.com.sg>
2020-03-10 11:35:16 +0800

    added react redux actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+
 import './index.css';
+
+
 import App from './App';
+import rootReducer from './reducers'
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-<App />, 
-document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
