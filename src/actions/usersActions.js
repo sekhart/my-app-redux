02ca8bb4c94f97066212d6fc2f6ce1425e5edb6f
sekhartsr@gmail.com <sekhartsr@gmail.com>
2021-07-07 17:25:19 +0800

    added DockerFile

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,36 +1,34 @@
 // Create Redux action types
-export const GET_USERS = 'GET GET_USERS'
-export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS'
-export const GET_USERS_FAILURE = 'GET_USERS_FAILURE'
+export const GET_USERS = "GET GET_USERS";
+export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS";
+export const GET_USERS_FAILURE = "GET_USERS_FAILURE";
 
 export const getUsers = () => ({
-    type: GET_USERS,
-})
+  type: GET_USERS,
+});
 
-
-export const getUsersSuccess = users => ({
-    type: GET_USERS_SUCCESS,
-    payload: users,
-})
-
+export const getUsersSuccess = (users) => ({
+  type: GET_USERS_SUCCESS,
+  payload: users,
+});
 
 export const getUsersFailure = () => ({
-    type: GET_USERS_FAILURE,
-})
-
+  type: GET_USERS_FAILURE,
+});
 
 export function fetchUsers() {
-    return async dispatch => {
-        dispatch(getUsers())
+  return async (dispatch) => {
+    dispatch(getUsers());
 
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users')
-            const data = await response.json()
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
 
-            dispatch(getUsersSuccess(data))
-
-        } catch (error) {
-            dispatch(getUsersFailure())
-        }
+      dispatch(getUsersSuccess(data));
+    } catch (error) {
+      dispatch(getUsersFailure());
     }
+  };
 }
